f75ad16b7f8fbb70413d1ade76668d04931ff3e2
Francisco Gillman <botturiperusso@gmail.com>
2025-09-03 16:38:36 -0300

    Create conexion.php

---
+++ PROYECTO FINAL/logica/conexion.php
@@ -0,0 +1,8 @@
+<?php
+$servidor = "127.0.0.1";
+$usuario = "root";
+$clave = "";
+$baseDatos = "bytenexus";
+$conexion = mysqli_connect ($servidor, $usuario, $clave, $baseDatos);
+echo "Conexion exitosa!!";
+?>
